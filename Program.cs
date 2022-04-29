26e934f814fb3ae3ea7421065171b5bbdee6ec22
student <student@ACADEMYNETPD03>
2022-04-29 18:02:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿
+using FileProductH;
+
+var fileProductH = new HelperO("C:\\PDO.csv.txt");
+
+var PDO = fileProductH.ReadAllProdottiDaOrdinare();
+
+Console.WriteLine("*********************");
+
+foreach (var prodottiDaOrdinare in PDO)
+{
+    Console.WriteLine($"Id prodotto: {prodottiDaOrdinare.ProdottoIdO} - Nome prodotto: {prodottiDaOrdinare.NomeProdottoO} - Quantità: {prodottiDaOrdinare.QuantitaO}");
+}
+
+Console.WriteLine("*********************");
+
+Console.WriteLine("Inserire Id prodotto");
+var prodottoIdO = Console.ReadLine();
+Console.WriteLine("Inserire il nome del prodotto");
+var nomeProdottoO = Console.ReadLine();
+Console.WriteLine("Inserire la quantità");
+var quantitaO = Console.ReadLine();
+
+var prodottiDaOrdinarenew = new ProdottiDaOrdinare();
+prodottiDaOrdinarenew.ProdottoIdO = prodottoIdO;
+prodottiDaOrdinarenew.NomeProdottoO = nomeProdottoO;
+prodottiDaOrdinarenew.QuantitaO = quantitaO;
+
+fileProductH.AddProdottiDaOrdinare(prodottiDaOrdinarenew);
+
+PDO = fileProductH.ReadAllProdottiDaOrdinare();
+
+
+Console.WriteLine("*********************");
+
+foreach (var contact in PDO)
+{
+    Console.WriteLine($"Id prodotto: {prodottiDaOrdinarenew.ProdottoIdO} - nome prodotto: {prodottiDaOrdinarenew.NomeProdottoO} - quantità: {prodottiDaOrdinarenew.QuantitaO}");
+}
+
+Console.WriteLine("*********************");
+
+
+Console.ReadLine();
